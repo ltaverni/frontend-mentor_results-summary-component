7cd55f647dc7b608a7b960a7fca3be8346433002
ltaverni <>
2026-03-23 16:52:35 +0100

    skeletton of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,67 @@
 import './App.css'
+import categories from './data.json'
+
+const Categories = () => {
+    return (
+        <div>
+            {categories.map((category) => {
+                return (
+                    <div>
+                        <img src={category.icon} ></img>
+                        <div>{category.category}</div>
+                        <div>{category.score} / 100</div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+const Summary = () => {
+    return (
+        <div>
+            <div>Summary</div>
+            <Categories />
+            <button>Continue</button>
+        </div>
+    )
+}
+
+const Score = () => {
+    return (
+        <div>
+            <div>NOTE</div>
+            <div>of 100</div>
+        </div>
+    )
+}
+
+const Results = () => {
+    return (
+        <div>
+            <div>Your Result</div>
+            <Score />
+            <div>RATE</div>
+            <div>You scored higher than 65% of the people who have taken the tests.</div>
+        </div>
+    )
+}
+
+const Card = () => {
+    return (
+        <div>
+            <Results />
+            <Summary />
+        </div>
+    )
+}
 
 const App = () => {
-  return (
-    <>
-        TEST
-    </>
-  )
+    return (
+        <>
+            <Card />
+        </>
+    )
 }
 
 export default App
